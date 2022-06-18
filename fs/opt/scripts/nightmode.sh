09ea953ee698f339adf5cd37ce4d72be3d4cc608
Yevhen Fastiuk <yevfast@gmail.com>
2022-06-19 00:45:23 +0300

    Fix software autonight mode
    Signed-off-by: Yevhen Fastiuk <yevfast@gmail.com>

--- fs/opt/scripts/nightmode.sh
+++ fs/opt/scripts/nightmode.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
 . /opt/scripts/common_functions.sh
+night_mode $1 # comment this line when test
 
-night_mode $1
+# TODO: test
+# fix: autonight sends disable/enable every time it starts. not good
+TIMER_PID_FILE=/tmp/nm_pid
+
+if [ ! -f $TIMER_PID_FILE ]; then
+    night_mode $1
+    (
+        sleep 60
+        rm $TIMER_PID_FILE
+    ) &
+    echo $! > $TIMER_PID_FILE
+else
+    kill -9 "$(cat $TIMER_PID_FILE)"
+    (
+        sleep 60
+        night_mode $1
+        rm $TIMER_PID_FILE
+    ) &
+    echo $! > $TIMER_PID_FILE
+fi
